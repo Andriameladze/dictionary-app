dba4b0c01ecaf072b10b1a939023315b46dbae46
Andria Meladze <andriameladze@Andrias-MacBook-Air.local>
2023-02-09 14:16:17 +0400

    add search style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DictionaryComponent } from './dictionary/dictionary.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DictionaryComponent
   ],
   imports: [
     BrowserModule,
